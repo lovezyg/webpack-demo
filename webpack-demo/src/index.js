41bd155ab46ed6b8613d9a3af0628f45de5c0dfb
jwq <sxpoplar@163.com>
2018-07-16 16:40:34 +0800

    webpack-dev-server

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,23 @@
 import _ from 'lodash';
+import printMe from './print.js';
 function component(){
 	var element = document.createElement('div');
+	var btn = document.createElement('button');
 	// element.innerHTML = _.join(['Hello','webpack'],'');
 	element.innerHTML = _.join(['I','Love','You'],' ');
+
+	btn.innerHTML = 'click me, check the cnsole';
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
 
 	return element;
 }
 document.body.appendChild(component());
+
+// if (module.hot){
+// 	module.hot.accept('./print.js',function(){
+// 		console.log('Accepting the updated printMe module!');
+// 		printMe();
+// 	})
+// }
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname,'dist')
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname,'dist')
+    }
 };
