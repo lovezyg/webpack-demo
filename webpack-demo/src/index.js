3ffb0974f339ba6d1ce05c9a17ba9b926290e602
jwq <sxpoplar@163.com>
2018-07-16 10:10:58 +0800

    delete resource for Output Management

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,21 +1,8 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
 function component(){
 	var element = document.createElement('div');
 	// element.innerHTML = _.join(['Hello','webpack'],'');
 	element.innerHTML = _.join(['I','Love','You'],' ');
-	element.classList.add('hello');
-
-	//add image to div
-	let MyIcon = new Image();
-	MyIcon.src=Icon;
-	var str='zyg';
-
-	element.appendChild(MyIcon);
-
-	console.log(Data);
 
 	return element;
 }
