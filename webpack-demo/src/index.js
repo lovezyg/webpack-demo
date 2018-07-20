b8561849ddd605ba6fca1f6849f9116f95ac0679
jwq <sxpoplar@163.com>
2018-07-20 11:31:02 +0800

    tree shaking

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,28 +1,11 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './style.css';
+import { cube } from './math.js';
 function component(){
-	var element = document.createElement('div');
-	var btn = document.createElement('button');
-	// element.innerHTML = _.join(['Hello','webpack'],'');
-	element.innerHTML = _.join(['I','Love','You'],' ');
-
-	btn.innerHTML = 'click me and check the console';
-	btn.classList.add('hello');
-	btn.onclick = printMe;
-
-	element.appendChild(btn);
+	var element = document.createElement('pre');
+	element.innerHTML = [
+		     'Hello webpack!',
+		     '3 cubed is equal to ' + cube(3)
+		   ].join('\n\n');
 
 	return element;
 }
-let element = component();
-document.body.appendChild(element);
-
-if (module.hot){
-	module.hot.accept('./print.js',function(){
-		console.log('Accepting the updated printMe module!');
-		document.body.removeChild(element);
-     	element = component(); // 重新渲染页面后，component 更新 click 事件处理
-     	document.body.appendChild(element);
-	})
-}
+document.body.appendChild(component());
